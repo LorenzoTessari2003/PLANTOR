a63bef74c0f4b55cbf435612da18ba3bdac5f0cd
Enrico Saccon <enrico.saccon@nitn.it>
2025-01-12 23:51:48 +0100

    Updated requirements and backend options

--- llm_ui/backend/run.py
+++ llm_ui/backend/run.py
@@ -4,15 +4,38 @@ from flask import Flask
 from flask_cors import CORS
 from app.routes import app_routes
 
+import sys, getopt
+
 app = Flask(__name__)
 CORS(app)
 
 # Register blueprints
 app.register_blueprint(app_routes)
 
-@app.route('/print1')
-def print1():
-    return "Hello World"
+
+def usage():
+    print("Options are: d{,ebug}, h{,elp}, p{ort}=<port>")
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = 5000
+    debug = False
+    try:
+        opt, args = getopt.getopt(sys.argv[1:], "dp:h", ["help", "debug", "port="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    for o, a in opt:
+        if o in ["-h", "--help"]:
+            usage()
+            sys.exit(0)
+        elif o in ["-p", "--port"]:
+            port = int(a)
+        elif o in ["-d", "--debug"]:
+            debug = True
+        else:
+            print("Option not recognized")
+
+    app.run(debug=debug, host="0.0.0.0", port=port)
