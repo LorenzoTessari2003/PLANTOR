f9994e45908c6b6805b0fc60595fe1364e65167f
icosac <enrico.saccon@unitn.it>
2025-01-13 17:46:29 +0100

    cleaning up

--- llm_ui/backend/app/__init__.py
+++ llm_ui/backend/app/__init__.py
@@ -1,7 +1,15 @@
-import os, sys
+import os
+import sys
 
 PLOP_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
+print(f"Adding {PLOP_PATH} to sys.path")
 sys.path.append(PLOP_PATH)
 
-PUBLIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "frontend", "public_generated")
+PUBLIC_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..",
+    "..",
+    "frontend",
+    "public_generated",
+)
 os.makedirs(PUBLIC_PATH, exist_ok=True)
